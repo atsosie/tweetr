b261e87663036e7738bd4fea1bc7d2aa7e993e0d
Amber Tsosie <atxambra@gmail.com>
2017-04-19 02:52:43 +0000

    Added CSS class 'error-text' so counter now turns red for negative count. Removed obsolete comments.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,15 @@
-// alert("Hello");
-
-// .on( events [, selector ] [, data ], handler )
-// $( "#dataTable tbody tr" ).on( "click", function() {
-//   console.log( $( this ).text() );
-// });
-
-// $("selector").method();
-
 $(document).ready(function() {
 
   $(".new-tweet textarea").on("keyup", function() {
     const charLimit = 140;
     const charUsed = $(this).val().length;
     const charRemaining = charLimit - charUsed;
-    $(this).siblings(".counter").text(charRemaining);
-    // if (charRemaining < 0) {
-    //   $("")
-    // }
+    const charCount = $(this).siblings(".counter").text(charRemaining);
+    if (charRemaining < 0) {
+      charCount.addClass("error-text");
+    } else {
+      charCount.removeClass("error-text");
+    }
   });
 
 });
