a4b6f1f4d684206edc4b1039e5762873d2181e7f
Amber Tsosie <atxambra@gmail.com>
2017-04-20 01:56:50 +0000

    Added 'compose' button that toggles new-tweet form on click. CSS needs improvement.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
 
   // take in a tweet object and return a formatted tweet <article>
-  function createTweetElement (tweetData) {
+  function createTweetElement(tweetData) {
     let dateCreated = moment(tweetData.created_at).fromNow();
     let formattedTweet =
       `<article>
@@ -32,38 +32,51 @@ $(document).ready(function() {
     return formattedTweet;
   }
 
+
   // add formatted tweet to section of past tweets
   function renderTweets(dataArr) {
-    // *** Add step: clear section for tweets ***
+    $("article").empty(); // *** After they're deleted, a bottom border remains ***
     dataArr.forEach(function(dataObj) {
       const $tweet = createTweetElement(dataObj);
       $("#tweet").prepend($tweet);
     });
   }
 
+
   // display submitted tweet along with all past tweets
   function loadTweets() {
     $.ajax({
       url: "/tweets/",
       method: "GET",
-      success: function(response) {
+      success: function (response) {
         renderTweets(response);
       }
     });
   }
 
+
+  // toggle compose tweet form
+  const $composeTweet = $(".new-tweet");
+
+  $("#compose").on("click", function() {
+    $composeTweet.slideToggle(200).find("textarea").focus();
+  });
+
+
   // event listener for submitting new tweet
-  $("form").on("submit", function (event) {
+  $("form").on("submit", function(event) {
     console.log("form data: ", $(this).serialize());
     event.preventDefault();
     let charCount = $("#tweet-text").val().length;
 
     if (charCount === 0) {
       alert("Oops. Try typing something before hitting submit.");
+      return;
     }
 
     if (charCount > 140) {
       alert("Keep your tweet under 140 characters.");
+      return;
     }
 
     $.ajax({
